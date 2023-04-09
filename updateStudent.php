2dfbcbe4398a9c33d9573b2be007f409bc44669a
Ravi069 <40270108+Ravi069@users.noreply.github.com>
2023-04-09 16:03:05 +0530

    Update updateStudent.php

--- updateStudent.php
+++ updateStudent.php
@@ -1,6 +1,12 @@
 <?php
 require "connectToDatabase.php";
 
+if($_POST["update"]==""){
+  echo "</br>";
+  echo "No Records Found To Update";
+  exit;
+}
+
 $sql="UPDATE students SET department='CS' WHERE id='$_POST[update]'";
 
 if ($conn->query($sql) === TRUE) {
@@ -11,6 +17,4 @@ if ($conn->query($sql) === TRUE) {
 }
 
 $conn->close();
-
 ?>
-
