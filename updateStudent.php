e8d90307fd65e6de86507c74db5ca7eb55435e89
Ravi069 <40270108+Ravi069@users.noreply.github.com>
2023-04-12 21:26:56 +0530

    Update updateStudent.php

--- updateStudent.php
+++ updateStudent.php
@@ -1,20 +1,25 @@
 <?php
 require "connectToDatabase.php";
 
-if($_POST["update"]==""){
+if(empty($_POST["update"])){
   echo "</br>";
   echo "No Records Found To Update";
   exit;
 }
 
-$sql="UPDATE students SET department='CS' WHERE id='$_POST[update]'";
+$sql="SELECT id FROM students where id = '$_POST[update]'";
 
-if ($conn->query($sql) === TRUE) {
+$result = mysqli_query($conn, $sql);
+echo "</br>";
+if (mysqli_num_rows($result) > 0) {
+  $update_query="UPDATE students SET department='CS' WHERE id='$_POST[update]'";
+
+if ($conn->query($update_query) === TRUE) {
   echo "</br>";
   echo "Data Updated successfully";
+} 
 } else {
-  echo "Error Updating table: " . $conn->error;
+  echo "No Records Found To Update: " . $conn->error;
 }
-
 $conn->close();
 ?>
