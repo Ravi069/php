1a0ca404e550208293823b77f137d3c6dc76060b
Ravi069 <40270108+Ravi069@users.noreply.github.com>
2023-04-12 21:25:43 +0530

    Update add_student.php

--- add_student.php
+++ add_student.php
@@ -1,13 +1,9 @@
 <?php
 require "connectToDatabase.php";
 
-if(($_POST["student_name"]=="") || ($_POST["department"]=="")){
-  echo "</br>";
-  echo "No Records Found To Add";
-  exit;
-}
+if(!empty($_POST["student_name"]) || !empty($_POST["department"])){
 
-$sql="INSERTs INTO students (student_name, department)
+$sql="INSERT INTO students (student_name, department)
 VALUES ('$_POST[student_name]', '$_POST[department]')";
 
 if ($conn->query($sql) === TRUE) {
@@ -15,6 +11,10 @@ if ($conn->query($sql) === TRUE) {
   echo "Data Inserted successfully";
 } else {
   echo "Error Adding into table: " . $conn->error;
+}
+}else{
+  echo "</br>";
+  echo "Add Student name and department ";
 }
 
 $conn->close();
