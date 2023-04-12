98e4544da4cf8d64163e3b08592de56237a92a47
Ravi069 <40270108+Ravi069@users.noreply.github.com>
2023-04-12 22:44:08 +0530

    Add files via upload

---
+++ arrays/associative_array.php
@@ -0,0 +1,16 @@
+<?php
+
+$names = array(
+"Captain" => "Ravi",
+"Keeper"  => "Raja",
+"Bowler"  => "Rana",
+"Umpire"  => "Ragu"
+);
+
+echo "<pre>";
+print_r($names);
+echo"</pre>";
+
+echo "$names[Captain]";
+
+?>
---
+++ arrays/indexed_array.php
@@ -0,0 +1,14 @@
+<?php
+
+$names = array(
+"Ravi",
+"Raja",
+"Rana",
+"Ragu"
+);
+
+echo "<pre>";
+print_r($names);
+echo"</pre>";
+
+?>
